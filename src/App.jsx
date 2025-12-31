29e2eb7319a7a639f26edf0af6f5100be9e67037
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2025-12-31 13:22:08 +0530

    Refactor App component to include context providers and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+import { CartProvider } from './context/CartContext';
+import { ShopProvider } from './context/ShopContext'; 
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import BackToTop from './Components/BackToTop';
 
 function App() {
   return (
-    <Router>
-      <AppRoutes />
-    </Router>
+    <CartProvider>
+      <ShopProvider> 
+        <Router>
+          <BackToTop />
+          <Navbar />
+          <AppRoutes />
+          <Footer />
+        </Router>
+      </ShopProvider>
+    </CartProvider>
   );
 }
 
